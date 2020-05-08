74fd12e976dda601c312a47937d0863fe291195d
Artemy Eskov <aeskov@bekitzur.com>
2020-05-09 02:13:57 +0300

    Add build configuration

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,6 @@
-module.exports = {}
+module.exports = {
+  productionSourceMap: false,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/' + process.env.npm_package_name + '/'
+    : '/',
+}
